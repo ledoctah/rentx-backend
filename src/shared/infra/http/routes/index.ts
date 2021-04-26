0f23601a556a73f4896758c282f17a2be07442a2
ledoctah <matheusferr2015@gmail.com>
2021-04-26 20:56:48 -0300

    added 404 error handling

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express';
 import users from '@modules/users/infra/http/routes/users.routes';
 import cars from '@modules/cars/infra/http/routes/cars.routes';
 import session from '@modules/users/infra/http/routes/session.routes';
+import AppError from '@shared/errors/AppError';
 
 const router = Router();
 
@@ -14,6 +15,10 @@ router.get('/', (_: Request, response: Response) => {
   return response.json({
     message: 'Welcome to RentX API',
   });
+});
+
+router.all('*', () => {
+  throw new AppError('Route not found', 404);
 });
 
 export default router;
